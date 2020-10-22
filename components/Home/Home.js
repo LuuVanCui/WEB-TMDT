fdc12c48c1fa1ffd0cd91e6e1b68ebead24b44f3
Luu Van Cui <luuvancui10a1516@gmail.com>
2020-10-22 17:42:56 +0700

    home

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,6 +3,7 @@ export default function Home() {
         <div>
             <h1>Day la noi dung</h1>
             <h2>Update home</h2>
+            <p>Cap nhat home de pull request</p>
         </div>
     );
 }
