1c6e50420acabd8e5cf0d86cb81a254cb327bf49
quangdai501 <72977901+quangdai501@users.noreply.github.com>
2020-10-23 18:33:07 +0700

    product-detail 1

---
+++ pages/product-detail.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import React, { Component } from 'react';
+
+import Layout from '../components/Layout/Layout';
+import ProductDetail from '../components/ProductDetail/ProductDetail';
+
+export default class ProductPage extends Component {
+    render() {
+        return (
+            <>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <title>Product Detail</title>
+                    <meta name="robots" content="noindex, follow" />
+                    <meta name="description" content="" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    {/* Favicon */}
+                    <link rel="shortcut icon" href="/images/favicon.ico" />
+                </Head>
+                <Layout>
+                  <ProductDetail />
+                </Layout>
+            </>
+        );
+    }
+}
