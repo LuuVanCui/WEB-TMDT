9a9abd931ff1d5665b2e91e9b543f6cc91b09980
luuvancui <cuiluu@realshare.vn>
2020-11-12 17:46:10 +0700

    create token in api login

--- server/controllers/login/auth.controller.js
+++ server/controllers/login/auth.controller.js
@@ -1,4 +1,5 @@
 
+const jwt = require('jsonwebtoken');
 const User = require('../../models/user.model');
 
 class LoginController{
@@ -6,16 +7,24 @@ class LoginController{
     // [POST] /auth/login
     async login(req, res, next) {
         const { username, password } = req.body;
-        
-        const user = await User.findOne({username, password});
-        
+        const user = await User.findOne({username , password})
         if (user) {
-            console.log(username);
-            res.json({username});
+            const token = jwt.sign(
+                {
+                    id: user._id,
+                    username: user.username
+                },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+            );
+            return res.status(200).json({
+                token,
+                userId: user._id,
+                username: user.username
+            });
         }
 
-        console.log('Login fail');
-        res.json({error: 'Wrong username or password!'});
+        return res.status(500).json({message: "Authentication error!"});
     }
 }
 
