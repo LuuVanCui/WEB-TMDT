1410485079484cfdfbe77cad8b94de6f2c2e2650
luuvancui <cuiluu@realshare.vn>
2020-12-18 11:57:12 +0700

    set cookie

--- client/src/store.js
+++ client/src/store.js
@@ -2,8 +2,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
 
-const initialState = {}
+const cartItems = Cookie.getJSON('cartItems') || [];
+
+const initialState = { cart: { cartItems } }
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
