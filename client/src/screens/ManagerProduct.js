fd7af373cf84a10971c6dd1946f0b354f31eb12b
Trung Hieu <50879127+NguyenHieu-NNKHL@users.noreply.github.com>
2020-12-19 13:46:56 +0700

    giao dien managerProduct

---
+++ client/src/screens/ManagerProduct.js
@@ -0,0 +1,98 @@
+export default function ManagerProduct(props){
+    return(
+            <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-plain">
+              <div className="card-header card-header-primary">
+                <h4 className="card-title mt-0"> Quản lí sản phẩm</h4>
+                <p className="card-category"> </p>
+              </div>
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-hover">
+                    <thead className>
+                      <tr><th>
+                          ID
+                        </th>
+                        <th>
+                          Tên sản phẩm
+                        </th>
+                        <th>
+                        Thể loại
+                        </th>
+                        <th>
+                        Giá
+                        </th>
+                        <th>
+                          Thao tác
+                        </th>
+                        
+                      </tr>
+                      </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          123
+                        </td>
+                        <td>
+                          Son 3ce
+                        </td>
+                        <td>
+                          Son thỏi
+                        </td>
+                        <td>
+                          300,000 vnđ
+                        </td>
+                        <td>
+                          <span><a href="#">Sửa</a>/<a href="#">Xóa</a></span>
+                          
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          123
+                        </td>
+                        <td>
+                          Son 3ce
+                        </td>
+                        <td>
+                          Son thỏi
+                        </td>
+                        <td>
+                          300,000 vnđ
+                        </td>
+                        <td>
+                          <span><a href="#">Sửa</a>/<a href="#">Xóa</a></span>
+                          
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          123
+                        </td>
+                        <td>
+                          Son 3ce
+                        </td>
+                        <td>
+                          Son thỏi
+                        </td>
+                        <td>
+                          300,000 vnđ
+                        </td>
+                        <td>
+                          <span><a href="#">Sửa</a>/<a href="#">Xóa</a></span>
+                          
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    );
+}
