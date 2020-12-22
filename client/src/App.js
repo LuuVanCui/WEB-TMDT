b1d27674efe747b057b8578cb758ec7d2dcbd212
luuvancui <cuiluu@realshare.vn>
2020-12-22 12:22:55 +0700

    update css

--- client/src/App.js
+++ client/src/App.js
@@ -125,7 +125,7 @@ function App() {
             <Route path="/admin/managerProduct" component={ManagerProduct} />
             <Route path="/admin/manage-user" component={ManageUserScreen} />
 
-            <footer className="footer fixed-bottom">
+            <footer className="footer spad">
                 <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
                     Nông sản 3 anh em
                 </div>
