cb120c6ab6a9ee1ed360ec8d6bf5b98294f2bc1c
luuvancui <cuiluu@realshare.vn>
2020-12-25 23:36:43 +0700

    send email nodejs

--- api/controllers/user/user.controller.js
+++ api/controllers/user/user.controller.js
@@ -1,25 +1,32 @@
 const User = require('../../models/user.model');
-const mailer = require('../../sendEmail');
+const nodemailer = require('nodemailer');
 
 class UserController {
     // [GET] - /api/users
     async getAllUsers(req, res) {
 
-        let sendMail = async (req, res) => {
-            try {
-                // Lấy data truyền lên từ form phía client
-                const { to, subject, body } = req.body
-                // Thực hiện gửi email
-                await mailer.sendMail(to, subject, body)
-                // Quá trình gửi email thành công thì gửi về thông báo success cho người dùng
-                res.send('<h3>Your email has been sent successfully.</h3>')
-            } catch (error) {
-                // Nếu có lỗi thì log ra để kiểm tra và cũng gửi về client
-                console.log(error)
-                res.send(error)
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'luuvancui10a1516@gmail.com',
+                pass: ''
             }
-        }
-        sendMail(req, res);
+        });
+
+        var mailOptions = {
+            from: 'luuvancui10a1516@gmail.com',
+            to: 'luuvancui9a2@gmail.com',
+            subject: 'Sending Email using Node.js',
+            text: 'That was easy!'
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
 
         try {
             const users = await User.find();
