1c5a889cea489b203c439aac4c596f78b2d8f540
luuvancui <cuiluu@realshare.vn>
2020-12-26 14:11:26 +0700

    .

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,7 @@
 import { USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstants";
 import Axios from 'axios';
 import Cookie from 'js-cookie';
+import { disconnect } from "mongoose";
 
 const signin = (email, password) => async (dispatch) => {
     dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -38,6 +39,10 @@ const listUsers = () => async (dispatch) => {
     } catch (error) {
         dispatch({ type: USER_LIST_FAIL, payload: error.message });
     }
+}
+
+const confirmEmail = (code) => async (dispatch) => {
+    
 }
 
 export { signin, register, listUsers };
---
+++ client/src/screens/ConfirmEmailScreen.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+export default function ConfirmEmailScreen(props) {
+
+  const [code, setCode] = useState(null);
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(confirmEmail(code));
+  };
+
+  return (
+    <div className="form" onSubmit={submitHandler}>
+      <form>
+        <ul className="form-container">
+          <li>
+            <h2>Nhập mã từ email</h2>
+          </li>
+          <p>NS3AE đã gửi 1 mã đến email của bạn. Nhập mã để kích hoạt tài khoản nhé!</p>
+          <li>
+            <label htmlFor="number">
+              Code
+          </label>
+            <input type="number" name="number" id="number"
+              placeholder="Nhập mã vào đây nè!" required onChange={e => setCode(e.target.value)}>
+            </input>
+          </li>
+          <Link to="" className="link">Gửi lại email</Link>
+          <li>
+            <button type="submit" className="button primary">Tiếp tục</button>
+          </li>
+          <li><Link to="/signin" className="link">Bạn đã có tài khoản? Đăng nhập nào!</Link></li>
+        </ul>
+      </form>
+    </div>
+  )
+}
