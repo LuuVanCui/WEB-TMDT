6cd7ec3a339bc28bf9383d6faded7bb188db010c
quangdai501 <72977901+quangdai501@users.noreply.github.com>
2020-12-28 23:51:34 +0700

    Update userActions.js

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,13 +1,10 @@
 import {
-    USER_LIST_FAIL,
-    USER_LIST_REQUEST,
-    USER_LIST_SUCCESS,
-    USER_REGISTER_FAIL,
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
-    USER_SIGNIN_FAIL,
-    USER_SIGNIN_REQUEST,
-    USER_SIGNIN_SUCCESS
+    USER_CONFIRM_EMAIL_FAIL,
+    USER_CONFIRM_EMAIL_REQUEST,
+    USER_CONFIRM_EMAIL_SUCCESS,
+    USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS,
+    USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,
+    USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_LOGOUT_SUCCESS, USER_FOGOT_PASSWORD_REQUEST, USER_FOGOT_PASSWORD_SUCCESS, USER_FOGOT_PASSWORD_FAIL
 } from "../constants/userConstants";
 import Axios from 'axios';
 import Cookie from 'js-cookie';
@@ -29,15 +26,28 @@ const signin = (email, password) => async (dispatch) => {
     }
 };
 
-const register = (name, email, password) => async (dispatch) => {
-    dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
+const register = (name = '', email = '', password = '') => async (dispatch, getState) => {
     try {
-        const { data } = await Axios.post('/api/auth/register', { name, email, password });
+        if (name === '' && email === '' && password === '') {
+            const { userRegister } = getState();
+            name = userRegister.userInfo.data.name;
+            email = userRegister.userInfo.data.email;
+            password = userRegister.userInfo.data.password;
+        }
+        dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
+        const { data } = await Axios.post('/api/auth/confirm-email', { name, email, password });
         dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-        Cookie.set('userInfo', JSON.stringify(data));
-    } catch (error) {
-        dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
     }
+    catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
+        });
+    }
+
 }
 
 const listUsers = () => async (dispatch) => {
@@ -46,13 +56,47 @@ const listUsers = () => async (dispatch) => {
         const { data } = await Axios.get('/api/users');
         dispatch({ type: USER_LIST_SUCCESS, payload: data });
     } catch (error) {
-        dispatch({ type: USER_LIST_FAIL, payload: error.message });
+        dispatch({
+            type: USER_LIST_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
+        });
     }
 }
 
 const confirmEmail = (code) => async (dispatch) => {
-
+    try {
+        dispatch({ type: USER_CONFIRM_EMAIL_REQUEST });
+        const { data } = await Axios.post('/api/users/add-user', { code });
+        dispatch({ type: USER_CONFIRM_EMAIL_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: USER_CONFIRM_EMAIL_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        });
+    }
 }
 
+const userLogOut = () => async (dispatch) => {
+    try {
+        Cookie.set('userInfo', '', 0);
+        dispatch({ type: USER_LOGOUT_SUCCESS, payload: {} });
+    } catch (error) {
 
-export { signin, register, listUsers };
+    }
+}
+
+const fogotPassword = (email) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_FOGOT_PASSWORD_REQUEST });
+        const { data } = await Axios.post('/api/auth/fogot-password', { email });
+        dispatch({ type: USER_FOGOT_PASSWORD_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: USER_FOGOT_PASSWORD_FAIL, error: error.response.data.message });
+    }
+}
+export { signin, register, listUsers, userLogOut, confirmEmail, fogotPassword };
